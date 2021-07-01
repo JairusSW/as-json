336dee6b8e77b5b5699cf84c6b76d15634b65ea5
JairusSW <jairus.v.tanaka@outlook.com>
2021-07-01 12:25:04 -0700

    @DogWhich (changes)

--- assembly/test.ts
+++ assembly/test.ts
@@ -9,3 +9,5 @@ strAny.set<string>('Hello!')
 arr.push(strAny)
 
 console.log(arr[0].get<string>())
+
+//My internet turns off in 14 min. I'll push and you can make a fork and continue locally. Ok?
--- transform/anyTransform/index.ts
+++ transform/anyTransform/index.ts
@@ -1,16 +1,36 @@
 import { Transform } from "assemblyscript/cli/transform";
 
 import {
-    Program
+    Program, VariableStatement, NamedTypeNode, NewExpression
 } from "assemblyscript";
 
 export = class MyTransform extends Transform {
     afterInitialize(program: Program): void {
         program.filesByName.forEach((file) => {
-            // 
+            program.managedClasses.forEach((c) => {
+                console.log(c.name)
+                if (c.name == "anyType") {
+                    console.log(c.prototype.instanceMembers?.size);
+                }
+            })
             if (!file.source.isLibrary) {
-                file.
-             }
+                file.source.statements.forEach(statement => {
+                    if (statement instanceof VariableStatement) {
+                        statement.declarations.forEach(declaration => {
+                            // console.log( NodeKind [declaration.kind ]);
+                            // console.log(NodeKind[declaration.initializer?.kind??(()=>{throw new Error("1");})]);
+                            if (declaration.initializer instanceof NewExpression) {
+                                console.log(declaration.initializer.typeName.identifier.text)
+                            }
+                            if (declaration.type instanceof NamedTypeNode) {
+                                console.log(declaration.type.name.identifier.text)
+                            }
+                        })
+
+                    }
+                })
+            }
         })
     }
 }
+// you here?
