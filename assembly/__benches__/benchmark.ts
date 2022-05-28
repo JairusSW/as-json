869ff04a8acf0d310f7d1511b2aecf95beb73dbd
JairusSW <jairus.v.tanaka@outlook.com>
2022-05-28 14:58:02 -0700

    Follow the advice of Lord rom

--- assembly/__benches__/benchmark.ts
+++ assembly/__benches__/benchmark.ts
@@ -10,58 +10,58 @@ const vec: Vector = {
     y: 0.0
 }
 
-bench("JSON Stringify String", () => {
+bench("Stringify String", () => {
     blackbox(JSON.stringify("Hello"))
 })
 
-bench("JSON Stringify Boolean", () => {
+bench("Stringify Boolean", () => {
     blackbox(JSON.stringify(true))
 })
 
-bench("JSON Stringify Integer", () => {
+bench("Stringify Integer", () => {
     blackbox(JSON.stringify(314))
 })
 
-bench("JSON Stringify Float", () => {
+bench("Stringify Float", () => {
     blackbox(JSON.stringify(3.14))
 })
 
-bench("JSON Stringify Vector", () => {
+bench("Stringify Vector", () => {
     blackbox(JSON.stringify(vec));
 });
 
-bench("JSON Stringify Array", () => {
+bench("Stringify Array", () => {
     blackbox(JSON.stringify([1,2,3,4,5]))
 })
 
-bench("JSON Parse String", () => {
+bench("Parse String", () => {
     blackbox(JSON.parse<string>("\"Hello\""))
 })
 
-bench("JSON Parse Boolean", () => {
+bench("Parse Boolean", () => {
     blackbox(JSON.parse<boolean>("true"))
 })
 
-bench("JSON Parse Integer", () => {
+bench("Parse Integer", () => {
     blackbox(JSON.parse<i32>("314"))
 })
 
-bench("JSON Parse Float", () => {
+bench("Parse Float", () => {
     blackbox(JSON.parse<f32>("3.14"))
 })
 
-bench("JSON Parse Vector", () => {
+bench("Parse Vector", () => {
     blackbox(parseMap<Map<string, f32>>(("{\"x\":0.0,\"y\":0.0}")))
 })
 
-bench("JSON Parse Boolean Array", () => {
+bench("Parse Boolean Array", () => {
     blackbox(parseBooleanArray<boolean[]>("[true,false,true,false,true]"))
 })
 
-bench("JSON Parse Integer Array", () => {
+bench("Parse Integer Array", () => {
     blackbox(parseNumberArray<u32[]>("[1,2,3,4,5]"))
 })
 
-bench("JSON Parse Float Array", () => {
+bench("Parse Float Array", () => {
     blackbox(parseNumberArray<f32[]>("[1.0,2.0,3.0,4.0,5.0]"))
 })
