b721f14624b342a62bb8f4e22feb0220d3048d3d
JairusSW <jairus.v.tanaka@outlook.com>
2022-05-30 08:23:07 -0700

    Fix Benchmarks

--- assembly/__benches__/benchmark.ts
+++ assembly/__benches__/benchmark.ts
@@ -5,25 +5,25 @@ class Vector {
     y: f32
 }
 
-const vec: Vector = {
+const vec: Vector = blackbox<Vector>({
     x: 0.0,
     y: 0.0
-}
+})
 
 bench("Stringify String", () => {
-    blackbox(JSON.stringify("Hello"))
+    blackbox(JSON.stringify(blackbox("Hello")))
 })
 
 bench("Stringify Boolean", () => {
-    blackbox(JSON.stringify(true))
+    blackbox(JSON.stringify(blackbox(true)))
 })
 
 bench("Stringify Integer", () => {
-    blackbox(JSON.stringify(314))
+    blackbox(JSON.stringify(blackbox(314)))
 })
 
 bench("Stringify Float", () => {
-    blackbox(JSON.stringify(3.14))
+    blackbox(JSON.stringify(blackbox(3.14)))
 })
 
 bench("Stringify Vector", () => {
@@ -31,37 +31,37 @@ bench("Stringify Vector", () => {
 });
 
 bench("Stringify Array", () => {
-    blackbox(JSON.stringify([1,2,3,4,5]))
+    blackbox(JSON.stringify(blackbox([1,2,3,4,5])))
 })
 
 bench("Parse String", () => {
-    blackbox(JSON.parse<string>("\"Hello\""))
+    blackbox(JSON.parse<string>(blackbox("\"Hello\"")))
 })
 
 bench("Parse Boolean", () => {
-    blackbox(JSON.parse<boolean>("true"))
+    blackbox(JSON.parse<boolean>(blackbox("true")))
 })
 
 bench("Parse Integer", () => {
-    blackbox(JSON.parse<i32>("314"))
+    blackbox(JSON.parse<i32>(blackbox("314")))
 })
 
 bench("Parse Float", () => {
-    blackbox(JSON.parse<f32>("3.14"))
+    blackbox(JSON.parse<f32>(blackbox("3.14")))
 })
 
 bench("Parse Vector", () => {
-    blackbox(parseMap<Map<string, f32>>(("{\"x\":0.0,\"y\":0.0}")))
+    blackbox(parseMap<Map<string, f32>>((blackbox("{\"x\":0.0,\"y\":0.0}"))))
 })
 
 bench("Parse Boolean Array", () => {
-    blackbox(parseBooleanArray<boolean[]>("[true,false,true,false,true]"))
+    blackbox(parseBooleanArray<boolean[]>(blackbox("[true,false,true,false,true]")))
 })
 
 bench("Parse Integer Array", () => {
-    blackbox(parseNumberArray<u32[]>("[1,2,3,4,5]"))
+    blackbox(parseNumberArray<u32[]>(blackbox("[1,2,3,4,5]")))
 })
 
 bench("Parse Float Array", () => {
-    blackbox(parseNumberArray<f32[]>("[1.0,2.0,3.0,4.0,5.0]"))
+    blackbox(parseNumberArray<f32[]>(blackbox("[1.0,2.0,3.0,4.0,5.0]")))
 })
