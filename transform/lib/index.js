eeb2ab0693c95befd81e4ec6a8a3f96419b6cff2
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-02-15 12:18:46 -0800

    build transform

--- transform/lib/index.js
+++ transform/lib/index.js
@@ -48,9 +48,9 @@ class AsJSONTransform extends BaseVisitor {
         //);
     }
     visitClassDeclaration(node) {
-        var _a;
+        var _c;
         const className = node.name.text;
-        if (!((_a = node.decorators) === null || _a === void 0 ? void 0 : _a.length))
+        if (!((_c = node.decorators) === null || _c === void 0 ? void 0 : _c.length))
             return;
         let foundDecorator = false;
         for (const decorator of node.decorators) {
@@ -127,8 +127,22 @@ export default class Transformer extends Transform {
     afterParse(parser) {
         // Create new transform
         const transformer = new AsJSONTransform();
+        // Sort the sources so that user scripts are visited last
+        const sources = parser.sources.filter(source => !isStdlib(source)).sort((_a, _b) => {
+            const a = _a.internalPath;
+            const b = _b.internalPath;
+            if (a[0] === "~" && b[0] !== "~") {
+                return -1;
+            }
+            else if (a[0] !== "~" && b[0] === "~") {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        });
         // Loop over every source
-        for (const source of parser.sources) {
+        for (const source of sources) {
             // Ignore all lib and std. Visit everything else.
             if (!isStdlib(source)) {
                 transformer.visit(source);
