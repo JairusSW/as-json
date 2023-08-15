74e5d875cf069de7086a67e56e218cbaa85d7fd4
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-08-15 12:59:30 -0700

    feat: nodejs wasm benchmarking

---
+++ bench/bench-node.js
@@ -0,0 +1,17 @@
+import { readFileSync } from "fs";
+import { WASI } from "wasi";
+import { argv, env } from 'node:process';
+
+const wasm = readFileSync("./benchmark.wasm");
+
+const wasi = new WASI({
+    version: 'preview1',
+    args: argv,
+    env,
+    preopens: {},
+});
+
+const mod = new WebAssembly.Module(wasm);
+const instance = new WebAssembly.Instance(mod, wasi.getImportObject());
+
+wasi.start(instance);
