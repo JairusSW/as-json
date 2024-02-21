4c83347cce07b7230029c22f24be7d7f9557fe83
Matt Johnson-Pint <mattjohnsonpint@gmail.com>
2024-02-21 10:20:36 -0800

    invert condition to reduce nesting

--- assembly/src/json.ts
+++ assembly/src/json.ts
@@ -351,60 +351,61 @@ export namespace JSON {
   let result = new StringSink();
   let last = 1;
   for (let i = 1; i < data.length - 1; i++) {
-    if (unsafeCharCodeAt(data, i) === backSlashCode) {
-      const char = unsafeCharCodeAt(data, ++i);
-      result.write(data, last, i - 1);
-      switch (char) {
-        case quoteCode: {
-          result.writeCodePoint(quoteCode);
-          last = i + 1;
-          break;
-        }
-        case backSlashCode: {
-          result.writeCodePoint(backSlashCode);
-          last = i + 1;
-          break;
-        }
-        case forwardSlashCode: {
-          result.writeCodePoint(forwardSlashCode);
-          last = i + 1;
-          break;
-        }
-        case bCode: {
-          result.writeCodePoint(backspaceCode);
-          last = i + 1;
-          break;
-        }
-        case fCode: {
-          result.writeCodePoint(formFeedCode);
-          last = i + 1;
-          break;
-        }
-        case nCode: {
-          result.writeCodePoint(newLineCode);
-          last = i + 1;
-          break;
-        }
-        case rCode: {
-          result.writeCodePoint(carriageReturnCode);
-          last = i + 1;
-          break;
-        }
-        case tCode: {
-          result.writeCodePoint(tabCode);
-          last = i + 1;
-          break;
-        }
-        case uCode: {
-          const code = u16.parse(data.slice(i + 1, i + 5), 16);
-          result.writeCodePoint(code);
-          i += 4;
-          last = i + 1;
-          break;
-        }
-        default: {
-          throw new Error(`JSON: Cannot parse "${data}" as string. Invalid escape sequence: \\${data.charAt(i)}`);
-        }
+    if (unsafeCharCodeAt(data, i) !== backSlashCode) {
+      continue;
+    }
+    const char = unsafeCharCodeAt(data, ++i);
+    result.write(data, last, i - 1);
+    switch (char) {
+      case quoteCode: {
+        result.writeCodePoint(quoteCode);
+        last = i + 1;
+        break;
+      }
+      case backSlashCode: {
+        result.writeCodePoint(backSlashCode);
+        last = i + 1;
+        break;
+      }
+      case forwardSlashCode: {
+        result.writeCodePoint(forwardSlashCode);
+        last = i + 1;
+        break;
+      }
+      case bCode: {
+        result.writeCodePoint(backspaceCode);
+        last = i + 1;
+        break;
+      }
+      case fCode: {
+        result.writeCodePoint(formFeedCode);
+        last = i + 1;
+        break;
+      }
+      case nCode: {
+        result.writeCodePoint(newLineCode);
+        last = i + 1;
+        break;
+      }
+      case rCode: {
+        result.writeCodePoint(carriageReturnCode);
+        last = i + 1;
+        break;
+      }
+      case tCode: {
+        result.writeCodePoint(tabCode);
+        last = i + 1;
+        break;
+      }
+      case uCode: {
+        const code = u16.parse(data.slice(i + 1, i + 5), 16);
+        result.writeCodePoint(code);
+        i += 4;
+        last = i + 1;
+        break;
+      }
+      default: {
+        throw new Error(`JSON: Cannot parse "${data}" as string. Invalid escape sequence: \\${data.charAt(i)}`);
       }
     }
   }
