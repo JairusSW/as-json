f17fe793d1cfc70e230338057c6102730eca0247
Jairus <me@jairus.dev>
2024-06-14 19:20:57 -0700

    feat: introduce JSON.Value

--- assembly/index.ts
+++ assembly/index.ts
@@ -18,14 +18,147 @@ import { deserializeBox } from "./deserialize/box";
 import { deserializeObject } from "./deserialize/object";
 import { deserializeMap } from "./deserialize/map";
 import { deserializeDate } from "./deserialize/date";
-import { nullWord } from "./src/chars";
+import { falseWord, nullWord, trueWord } from "./src/chars";
 import { deserializeInteger } from "./deserialize/integer";
 import { deserializeString } from "./deserialize/string";
+import { Sink } from "./src/sink";
+import { Variant } from "as-variant/assembly";
+
+/**
+ * Offset of the 'storage' property in the JSON.Value class.
+ */
+// @ts-ignore: Decorator valid here
+@inline const STORAGE = offsetof<JSON.Value>("storage");
 
 /**
  * JSON Encoder/Decoder for AssemblyScript
  */
 export namespace JSON {
+  /**
+ * Enum representing the different types supported by JSON.
+ */
+  export enum Types {
+    U8,
+    U16,
+    U32,
+    U64,
+    F32,
+    F64,
+    Boolean,
+    String,
+    Obj,
+    Array
+  }
+  export class Value {
+    public type: i32;
+    public length: i32 = 0;
+
+    // @ts-ignore: storage is set directly through memory
+    private storage: u64;
+
+    private constructor() { unreachable(); }
+
+    /**
+     * Creates an JSON.Value instance from a given value.
+     * @param value - The value to be encapsulated.
+     * @returns An instance of JSON.Value.
+     */
+    @inline static from<T>(value: T): JSON.Value {
+      if (value instanceof Variant) {
+        // Handle
+      } else if (value instanceof JSON.Value) {
+        return value;
+      }
+      const out = changetype<JSON.Value>(__new(offsetof<JSON.Value>(), idof<JSON.Value>()));
+      out.set<T>(value);
+      return out;
+    }
+
+    /**
+     * Sets the value of the JSON.Value instance.
+     * @param value - The value to be set.
+     */
+    @inline set<T>(value: T): void {
+      if (isBoolean<T>()) {
+        this.type = JSON.Types.Boolean;
+        store<T>(changetype<usize>(this), value, STORAGE);
+      } else if (value instanceof u8 || value instanceof i8) {
+        this.type = JSON.Types.U8;
+        store<T>(changetype<usize>(this), value, STORAGE);
+      } else if (value instanceof u16 || value instanceof i16) {
+        this.type = JSON.Types.U16;
+        store<T>(changetype<usize>(this), value, STORAGE);
+      } else if (value instanceof u32 || value instanceof i32) {
+        this.type = JSON.Types.U32;
+        store<T>(changetype<usize>(this), value, STORAGE);
+      } else if (value instanceof u64 || value instanceof i64) {
+        this.type = JSON.Types.U64;
+        store<T>(changetype<usize>(this), value, STORAGE);
+      } else if (value instanceof f32) {
+        this.type = JSON.Types.F64;
+        store<T>(changetype<usize>(this), value, STORAGE);
+      } else if (value instanceof f64) {
+        this.type = JSON.Types.F64;
+        store<T>(changetype<usize>(this), value, STORAGE);
+      } else if (isString<T>()) {
+        this.type = JSON.Types.String;
+        this.length = String.UTF8.byteLength(value as string);
+        store<T>(changetype<usize>(this), value, STORAGE);
+      } else if (value instanceof Map) {
+        if (idof<T>() !== idof<Map<string, JSON.Value>>()) {
+          abort("Maps must be of type Map<string, JSON.Value>!");
+        }
+        this.type = JSON.Types.Obj;
+        store<T>(changetype<usize>(this), value, STORAGE);
+      } else if (isArray<T>()) {
+        // @ts-ignore: T satisfies constraints of any[]
+        this.type = JSON.Types.Array + getArrayDepth<T>(0);
+        // @ts-ignore
+        this.length = value.length;
+        store<T>(changetype<usize>(this), value, STORAGE);
+      }
+    }
+
+    /**
+     * Gets the value of the JSON.Value instance.
+     * @returns The encapsulated value.
+     */
+    @inline get<T>(): T {
+      return load<T>(changetype<usize>(this), STORAGE);
+    }
+
+    /**
+     * Converts the JSON.Value to a string representation.
+     * @param useString - If true, treats Buffer as a string.
+     * @returns The string representation of the JSON.Value.
+     */
+    toString(useString: boolean = false): string {
+      switch (this.type) {
+        case JSON.Types.U8: return this.get<u8>().toString();
+        case JSON.Types.U16: return this.get<u16>().toString();
+        case JSON.Types.U32: return this.get<u32>().toString();
+        case JSON.Types.U64: return this.get<u64>().toString();
+        case JSON.Types.String: return "\"" + this.get<string>() + "\"";
+        case JSON.Types.Boolean: return this.get<boolean>() ? trueWord : falseWord;
+        default: {
+          const arr = this.get<JSON.Value[]>();
+          if (!arr.length) return "[]";
+          const out = Sink.fromString("[");
+          for (let i = 0; i < arr.length - 1; i++) {
+            const element = unchecked(arr[i]);
+            out.write(element.toString(useString));
+            out.write(",");
+          }
+
+          const element = unchecked(arr[arr.length - 1]);
+          out.write(element.toString(useString));
+
+          out.write("]");
+          return out.toString();
+        }
+      }
+    }
+  }
   /**
    * Stringifies valid JSON data.
    * ```js
@@ -113,14 +246,7 @@ export namespace JSON {
   }
 }
 
-// @ts-ignore: Decorator
-@global @inline function __parseObjectValue<T>(data: string, initializeDefaultValues: boolean): T {
-  // @ts-ignore
-  if (isString<T>()) return data;
-  return JSON.parse<T>(data, initializeDefaultValues);
-}
-
-// Dirty fix
+// This allows JSON.stringify and JSON.parse to be available globally through an alias
 // @ts-ignore: Decorator
 @global @inline function __SERIALIZE<T>(data: T): string {
   return JSON.stringify(data);
