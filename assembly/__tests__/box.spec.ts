4f66c67ef81015d1a3d83f95477b70ed6db40410
Jairus <me@jairus.dev>
2025-02-03 16:25:37 -0500

    tests: add boolean tests to Box<T>

--- assembly/__tests__/box.spec.ts
+++ assembly/__tests__/box.spec.ts
@@ -4,10 +4,18 @@ import { describe, expect } from "../../modules/test/assembly";
 describe("Should serialize JSON.Box<T>", () => {
   expect(JSON.stringify<JSON.Box<i32> | null>(null))
     .toBe("null");
+
   expect(JSON.stringify<JSON.Box<i32> | null>(new JSON.Box<i32>(0)))
     .toBe("0");
+
   expect(JSON.stringify<JSON.Box<i32> | null>(new JSON.Box<i32>(1)))
     .toBe("1");
+
+  expect(JSON.stringify<JSON.Box<boolean> | null>(new JSON.Box<boolean>(false)))
+    .toBe("false");
+
+  expect(JSON.stringify<JSON.Box<boolean> | null>(new JSON.Box<boolean>(true)))
+    .toBe("true");
 });
 
 // This is somewhat clumsy to use. Perhaps I can redesign it or use some transform to make it more transparent.
@@ -20,4 +28,10 @@ describe("Should deserialize JSON.Box<T>", () => {
 
   expect(JSON.parse<JSON.Box<i32> | null>("1")!.value.toString())
     .toBe("1");
+
+  expect(JSON.parse<JSON.Box<boolean> | null>("false")!.value.toString())
+    .toBe("false");
+
+  expect(JSON.parse<JSON.Box<boolean> | null>("true")!.value.toString())
+    .toBe("true");
 });
