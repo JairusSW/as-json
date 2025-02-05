d856069b30039ffffb69c0450cd73e8c0aa2c51a
Jairus <me@jairus.dev>
2025-02-05 18:12:06 -0500

    export snp function

--- assembly/util/snp.ts
+++ assembly/util/snp.ts
@@ -7,7 +7,7 @@ import { POW_TEN_TABLE_32, POW_TEN_TABLE_64 } from "../globals/tables";
 import { atoi } from "./atoi";
 
 // @ts-ignore: Decorator valid here
-@inline function snp<T extends number>(srcStart: usize, srcEnd: usize): T {
+@inline export function snp<T extends number>(srcStart: usize, srcEnd: usize): T {
   // @ts-ignore: type
   let val: T = 0;
   let char = load<u16>(srcStart) - 48;
@@ -46,7 +46,7 @@ import { atoi } from "./atoi";
           char = load<u16>(srcStart);
           if (char == 45) {
             // @ts-ignore: type
-            return val / pow10(atoi(srcStart + 2, srcEnd));
+            return val / pow10(atoi<u8>(srcStart + 2, srcEnd));
           } else {
             // @ts-ignore: type
             return val * pow10(atoi(srcStart, srcEnd));
