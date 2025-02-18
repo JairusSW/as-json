e0e53c118758f2b8b844fefa33e8fcb927e60ba2
Jairus <me@jairus.dev>
2025-02-18 17:15:50 -0500

    fix: boolean type not breaking

--- assembly/serialize/simple/arbitrary.ts
+++ assembly/serialize/simple/arbitrary.ts
@@ -1,6 +1,7 @@
 import { JSON } from "../..";
 import { serializeArray } from "./array";
 import { serializeBool } from "./bool";
+import { serializeFloat } from "./float";
 import { serializeInteger } from "./integer";
 import { serializeString } from "./string";
 
@@ -18,19 +19,26 @@ export function serializeArbitrary(src: JSON.Value): void {
     case JSON.Types.U64:
       serializeInteger<u64>(src.get<u64>());
       break;
+    case JSON.Types.F32:
+      serializeFloat<f32>(src.get<f32>());
+      break;
+    case JSON.Types.F64:
+      serializeFloat<f64>(src.get<f64>());
+      break;
     case JSON.Types.String:
       serializeString(src.get<string>());
       break;
     case JSON.Types.Bool:
       serializeBool(src.get<bool>());
+      break;
     case JSON.Types.Array: {
       serializeArray(src.get<JSON.Value[]>());
       break;
     }
     default: {
       const fn = JSON.Value.METHODS.get(src.type - JSON.Types.Struct);
-      const value = src.get<usize>();
-      call_indirect<string>(fn, 0, value);
+      const ptr = src.get<usize>();
+      call_indirect<void>(fn, 0, ptr);
     }
   }
 }
