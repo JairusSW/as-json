a534b388a138d70ba54240f270813a121357087a
Jairus <me@jairus.dev>
2025-03-04 23:40:36 -0500

    fix: wrong pointer types in dst

--- assembly/deserialize/simple/arbitrary.ts
+++ assembly/deserialize/simple/arbitrary.ts
@@ -4,14 +4,15 @@ import { deserializeBoolean } from "./bool";
 import { deserializeFloat } from "./float";
 import { deserializeString } from "./string";
 import { deserializeObject } from "./object";
+import { BRACE_LEFT, BRACKET_LEFT, QUOTE } from "../../custom/chars";
 
 export function deserializeArbitrary(srcStart: usize, srcEnd: usize, dst: usize): JSON.Value {
   const firstChar = load<u16>(srcStart);
-  if (firstChar == 34) return JSON.Value.from(deserializeString(srcStart, srcEnd, dst));
-  else if (firstChar == 123) return JSON.Value.from(deserializeObject(srcStart, srcEnd, dst));
+  if (firstChar == QUOTE) return JSON.Value.from(deserializeString(srcStart, srcEnd, 0));
+  else if (firstChar == BRACE_LEFT) return JSON.Value.from(deserializeObject(srcStart, srcEnd, 0));
   else if (firstChar - 48 <= 9 || firstChar == 45) return JSON.Value.from(deserializeFloat<f64>(srcStart, srcEnd));
-  else if (firstChar == 91) {
-    return JSON.Value.from(deserializeArray<JSON.Value[]>(srcStart, srcEnd, dst));
+  else if (firstChar == BRACKET_LEFT) {
+    return JSON.Value.from(deserializeArray<JSON.Value[]>(srcStart, srcEnd, 0));
   } else if (firstChar == 116 || firstChar == 102) return JSON.Value.from(deserializeBoolean(srcStart, srcEnd));
   return unreachable();
 }
